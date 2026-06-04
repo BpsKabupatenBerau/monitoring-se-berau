84c4544e4e9bc6982277f9a957c6594bc25d9b7b
BpsKabupatenBerau <inovasibps6405@gmail.com>
2026-06-04 18:12:39 +0700

    update: PWA version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+
+        manifest: {
+          name: 'MonSE 2026',
+          short_name: 'MonSE',
+
+          description: 'Monitoring Lapangan Sensus Ekonomi 2026 BPS Kabupaten Berau',
+
+          theme_color: '#0f172a',
+          background_color: '#ffffff',
+
+          display: 'standalone',
+          
+          start_url: '/',
+          icons: [{
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },],
+        },
+      },),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
